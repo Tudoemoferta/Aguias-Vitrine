7f7c8a8c87bfccd59df403bcab975d8b9236a294
Águias Vitrine - Tudo em Oferta <tudoemoferta123@gmail.com>
2026-01-21 16:18:17 -0400

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta name="description" content="Ofertas selecionadas por Águias Vitrine" />
+        <meta name="keywords" content="promoções, ofertas, afiliados, shopee, hotmart" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
