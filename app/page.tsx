e97f16a08e5d318059689275122236a8bb6150b2
Águias Vitrine - Tudo em Oferta <tudoemoferta123@gmail.com>
2026-01-21 17:35:57 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: "100vh",
+      backgroundImage: "url('/aguia.jpg')",
+      backgroundSize: "cover",
+      color: "#fff",
+      padding: "40px"
+    }}>
+      <h1>Águias Vitrine</h1>
+      <p>Vitrine premium de produtos selecionados</p>
+    </main>
+  );
+}
